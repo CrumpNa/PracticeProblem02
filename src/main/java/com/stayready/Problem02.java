6a13d45388b01f547ba18c5f6979e8aaac98b962
CrumpNa <nlcrump99@gmail.com>
2020-07-28 17:45:51 -0400

    prob 2

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,22 @@
 package com.stayready;
+import java.util.Arrays;
 
 public class Problem02 {
+
+
+
+
+    public String sort(int [] arr){
+        Arrays.sort(arr); //sort the array
+        String array=arr.toString();
+        return array;
+
+    }
+
+
+    public int howManyContinuous(){
+            return 0;
+    }
 }
+
+
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,29 @@
 package com.stayready;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Problem02Test {
+    Problem02 problem02= new Problem02();
+
+    @Test
+    public void sortTest(){
+        int [] arr= {6,1,2,5,4};
+        String expectedArr= "{1,2,4,5,6}";
+        String actualArr=problem02.sort(arr);
+        Assert.assertEquals(expectedArr,actualArr);
+    }
+
+    /*
+   @Test
+   public void howManyContinuousTest(){
+       int [] arr={1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+       int expectedAmt=5;
+       int actualAmt=problem02.howManyContinuous(arr);
+       Assert.assertEquals(expectedAmt, actualAmt);
+
+
+   }
+
+*/
+
 }
